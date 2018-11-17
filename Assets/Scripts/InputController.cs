c11e26664e7abc5f780e60cf18845c793932a9ad
Matteo De Benedetti <MatteoDeBenedetti@users.noreply.github.com>
2018-11-17 17:33:02 +0100

    Improved zoom
    zoom is now obtained with a setpoint and a Lerp interpolation, resulting in a smoother zoom

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,18 +6,31 @@ public class InputController : MonoBehaviour
 {
     Camera mainCamera;
 
-    float scale = 5f;
+    float scale = 3f;
+    float zoomSmoothfactor = 10f; //0.1f;
+    float newCameraSize;
 
 	void Start () 
 	{
         mainCamera = FindObjectOfType<Camera>();
-	}
+        newCameraSize = mainCamera.orthographicSize;
+
+    }
 	
 	void Update () 
 	{
-        if ( Input.mouseScrollDelta.y != 0)
+        if (Input.mouseScrollDelta.y != 0)
         {
-            mainCamera.orthographicSize -= Input.mouseScrollDelta.y * scale;
+            newCameraSize =
+                mainCamera.orthographicSize - Input.mouseScrollDelta.y * scale;
+        }
+
+        if (Mathf.Abs(newCameraSize - mainCamera.orthographicSize) > 0.1)
+        { 
+            mainCamera.orthographicSize = Mathf.Lerp(
+                mainCamera.orthographicSize, 
+                newCameraSize, 
+                zoomSmoothfactor * Time.deltaTime);
         }
     }
 }
