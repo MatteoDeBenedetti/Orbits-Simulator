fe176b031e5e1cb59031fa9e0c964272a747c2b5
Matteo De Benedetti <MatteoDeBenedetti@users.noreply.github.com>
2018-11-17 17:10:10 +0100

    camera zoom added

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour 
+{
+    Camera mainCamera;
+
+    float scale = 5f;
+
+	void Start () 
+	{
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	void Update () 
+	{
+        if ( Input.mouseScrollDelta.y != 0)
+        {
+            mainCamera.orthographicSize -= Input.mouseScrollDelta.y * scale;
+        }
+    }
+}
