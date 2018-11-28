51d99415d9dadf5ad55fd426062b7a6469ab9570
Matteo De Benedetti <MatteoDeBenedetti@users.noreply.github.com>
2018-11-28 19:13:11 +0100

    new spacecraft icon
    added a spacecraft icon with constant size independently from the zoom

---
+++ Assets/Scripts/SpacecraftIcon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpacecraftIcon : MonoBehaviour 
+{
+
+    Transform spacraftTransform;
+
+	void Start () 
+	{
+        spacraftTransform = FindObjectOfType<Spacecraft>().transform;
+	}
+	
+	void Update () 
+	{
+        transform.position = spacraftTransform.position;
+	}
+}
